265a99d6ff51a4fbe928dcd345ef1578e64d9829
anandadagit <130034415+anandadagit@users.noreply.github.com>
2024-10-20 21:34:19 +0000

    multiples of 5 completed

--- multiple-of-five/solution.js
+++ multiple-of-five/solution.js
@@ -1,14 +1,8 @@
-function getLastDigit(num) {
-  return num.toString()[num];
-}
 
 function isMultipleOfFive(n) {
-  console.log("The value of n -->", n);
-  if (getLastDigit(n) === 5 || getLastDigit(n) === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return n % 5 === 0;
 }
 
 module.exports = { isMultipleOfFive };
+
+console.log(isMultipleOfFive(5));
