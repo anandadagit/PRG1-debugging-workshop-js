cb97ed8e5888b32a5f6d2cf081412dceca9a9e28
anandadagit <130034415+anandadagit@users.noreply.github.com>
2024-10-20 20:33:11 +0000

    rotateAngle resolved

--- rotate-angle/solution.js
+++ rotate-angle/solution.js
@@ -1,6 +1,7 @@
 function rotateAngleByDegrees(initialAngle, rotation) {
   // Calculate the new angle
   let newAngle = initialAngle + (rotation % 360);
+  return newAngle < 0 ? newAngle + 360 : newAngle;
 
   return newAngle;
 }
--- times-table/solution.js
+++ times-table/solution.js
@@ -1,9 +1,13 @@
 function printTimesTable(n) {
   let i = 0;
-  while (i <= n) {
+  /*while (i <= n) {
     console.log(`${i} x ${n} = ${i * 5}`);
     i++;
+  }*/
+  for(let multiple=1; multiple < 13; multiple++) {
+    console.log(`${multiple} x ${n} = ${multiple * n}`);
   }
 }
 
 module.exports = { printTimesTable };
+printTimesTable(5);
