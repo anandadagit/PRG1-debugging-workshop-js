018276a319ab3aebf4959632224503293cc7966c
anandadagit <130034415+anandadagit@users.noreply.github.com>
2024-10-20 19:42:16 +0000

    12 hour clock solved

--- 12-hour-clock/solution.js
+++ 12-hour-clock/solution.js
@@ -1,13 +1,22 @@
 function convertTo12HourClock(time) {
-  const hours = time.slice(0, 2);
-  const minutes = time.slice(-2);
-  if (hours > 12) {
-    return `${hours - 12}:${minutes} PM`;
-  } else {
-    return `${hours}:${minutes} AM`;
+  let [hours,minutes] = time.split(':');
+  const period = hours >= 12 ? 'PM' : 'AM';
+  //let hour = hours%12 || 12;
+  let hour=0;
+  if(hours == 0){
+    hour = 12;
   }
+  else if(hours > 12) {
+    hour = parseInt(hours - 12);
+  }
+  else {
+    hour = parseInt(hours);
+  }
+  return `${hour}:${minutes} ${period}`;
+
 }
 
 module.exports = { convertTo12HourClock };
 
 // Hint: I recommend calling the function with an input of '12:05'
+console.log(convertTo12HourClock('00:00'));
--- pence-to-pounds/solution.js
+++ pence-to-pounds/solution.js
@@ -1,8 +1,8 @@
 function convertFromPenceToPounds(pence) {
   const pounds = pence / 100;
-  return `£${Math.round(pounds)}`; // Format to two decimal places
+  return `£${pounds.toFixed(2)}`; // Format to two decimal places
 }
 
 module.exports = { convertFromPenceToPounds };
 
-convertFromPenceToPounds(1299); // should return "£12.99"
+console.log(convertFromPenceToPounds(1299)); // should return "£12.99"
